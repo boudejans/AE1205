bd658d54734c4d2592e01f1c902e76f2dfcd1616
boudejans <bas.oudejans@kpnmail.nl>
2023-04-23 18:44:52 +0200

    Basic plane game

--- main.py
+++ main.py
@@ -1,16 +1,143 @@
-# This is a sample Python script.
+import pygame
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 720
+pixelsPerMeter = 7.2
+
+density = 1.225
+g0 = 9.81*7.2
+pressure = 101325
+temperature = 288.15
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
+        self.original_image = pygame.image.load("plane.png").convert()
+        self.original_image = pygame.transform.scale(self.original_image, (50, 50))
+        self.image = self.original_image
+        self.rect = self.image.get_rect().move(100, 100)
+        self.angle = 0
+        self.velocity_angle = 0
+        self.rotSpeedBase = 0.8
+        self.pos = [50, 50]
+        self.pos_x = self.pos[0]
+        self.pos_y = self.pos[1]
+        self.mass = 1000
+        self.thrust = 0
+        self.acceleration = 2000
+        self.maxThrust = 80000
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.x_heading = math.sin(math.radians(self.angle))
+        self.y_heading = math.cos(math.radians(self.angle))
+        self.velocity = 0
+
+    def accelerate(self, acceleration):
+        x = self.x_heading * acceleration
+        y = self.y_heading * acceleration
+        # print(str(x) + " " + str(y))
+        self.velocity_y += y
+        self.velocity_x += x
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def turn(self):
+        self.x_heading = math.sin(math.radians(self.angle))
+        self.y_heading = math.cos(math.radians(self.angle))
+        if self.y_heading < 0:
+            self.velocity_y = self.velocity * self.y_heading
+        else:
+            self.velocity_y = -self.velocity * self.y_heading
+        self.velocity_x = -self.velocity * self.x_heading
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        print(self.y_heading)
+
+    def update(self, pressed_keys):
+        self.rotSpeed = self.rotSpeedBase + self.rotSpeedBase * self.velocity / 700
+
+        if pressed_keys[K_UP]:
+            self.thrust += self.acceleration
+        if pressed_keys[K_DOWN]:
+            self.thrust -= self.acceleration
+
+        if self.thrust > self.maxThrust:
+            self.thrust = self.maxThrust
+        elif self.thrust < 0:
+            self.thrust = 0
+
+        y_acc = (((self.mass * g0) - (self.y_heading * self.thrust)) / self.mass) * dt
+        self.velocity_y += y_acc
+
+        x_acc = -self.x_heading * self.thrust / self.mass * dt
+        self.velocity_x += x_acc
+
+        self.velocity = math.sqrt(self.velocity_x ** 2 + self.velocity_y ** 2)
+
+        self.pos_x += self.velocity_x * dt
+        self.pos_y += self.velocity_y * dt
+        self.rect.center = (self.pos_x, self.pos_y)
+
+        if pressed_keys[K_LEFT]:
+            self.image = pygame.transform.rotate(self.original_image, self.angle)
+            self.angle += self.rotSpeed
+            x, y = self.rect.center
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+            self.turn()
+            # self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.image = pygame.transform.rotate(self.original_image, self.angle)
+            self.angle -= self.rotSpeed
+            x, y = self.rect.center
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+            self.turn()
+
+
+pygame.init()
+
+# font = pygame.font.Font('freesansbold.ttf', 32)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
+
+running = True
+
+while running:
+    screen.fill((0, 0, 0))
+
+    clock = pygame.time.Clock()
+    dt = clock.tick(60)/1000
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    font = pygame.font.Font(None, 30)
+    text = font.render(str(round(player.velocity, 2)) + " m/s", True, (255, 255, 255))
+    text2 = font.render(str(round(player.thrust, 2)) + " N", True, (255, 255, 255))
+    screen.blit(text, [10,10])
+    screen.blit(text2, [10, 50])
+
+    screen.blit(player.image, player.rect)
+
+    pygame.display.flip()
---
+++ main2.py
@@ -0,0 +1,91 @@
+import pygame
+import math
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 720
+
+density = 1.225
+g0 = 9.81
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Player, self).__init__()
+        self.original_image = pygame.image.load("plane.png").convert()
+        self.original_image = pygame.transform.scale(self.original_image, (50,50))
+        self.image = self.original_image
+        self.rect = self.image.get_rect().move(100,100)
+        self.angle = 0
+        self.rotSpeed = 0.1
+        self.pos = [50,50]
+        self.pos_x = self.pos[0]
+        self.pos_y = self.pos[1]
+
+    def velocity(self, velocity):
+
+        x = math.sin(math.radians(self.angle)) * velocity
+        y = math.cos(math.radians(self.angle)) * velocity
+        # print(str(x) + " " + str(y))
+        return [x,y]
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.pos_x -= self.velocity(0.2)[0]
+            self.pos_y -= self.velocity(0.2)[1]
+        if pressed_keys[K_DOWN]:
+            self.pos_x += self.velocity(0.2)[0]
+            self.pos_y += self.velocity(0.2)[1]
+
+        if pressed_keys[K_LEFT]:
+            self.image = pygame.transform.rotate(self.original_image, self.angle)
+            self.angle += self.rotSpeed
+            x,y = self.rect.center
+            self.rect = self.image.get_rect()
+            self.rect.center = (x,y)
+            # self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.image = pygame.transform.rotate(self.original_image, self.angle)
+            self.angle -= self.rotSpeed
+            x, y = self.rect.center
+            self.rect = self.image.get_rect()
+            self.rect.center = (x, y)
+
+        self.rect.center = (self.pos_x, self.pos_y)
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
+
+running = True
+clock = pygame.time.Clock()
+clock.tick(30)
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    screen.fill((0, 0, 0))
+
+    screen.blit(player.image, player.rect)
+
+    pygame.display.flip()
