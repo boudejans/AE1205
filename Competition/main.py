0f52b898d4d0f971986c7381599521d6e776cb02
boudejans <bas.oudejans@kpnmail.nl>
2023-06-10 18:45:00 +0200

    Final bug fix: final version of the game

--- Competition/main.py
+++ Competition/main.py
@@ -4,9 +4,12 @@ import world
 # Init pygame and the screen
 pygame.init()
 
+# Set screen size
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+# Uncomment for fullscreen: (Might mess up screen size)
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN, pygame.RESIZABLE)
 info = pygame.display.Info()
 SCREEN_WIDTH = info.current_w
 SCREEN_HEIGHT = info.current_h
